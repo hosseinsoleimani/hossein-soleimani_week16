fc99ecc16fd4ba26b04a053c5939d181dce11696
HosseinSoleimani <mrhosseinsoleimani@gmail.com>
2026-02-14 21:47:51 +0330

    input musk

---
+++ src/main.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+/* eslint-disable react/prop-types */
+const Input = ({ hint }) => {
+  const cities = [
+    "Tehran",
+    "Shiraz",
+    "Isfahan",
+    "Kermanshah",
+    "Ahwaz",
+    "Arak",
+    "Mashhad",
+    "Tabriz",
+  ];
+  const [suggestion, setSuggestion] = useState([]);
+
+  const handleChange = (event) => {
+    const inputValue = event.target.value;
+    if (inputValue === "") {
+      setSuggestion([]);
+      return;
+    }
+    const searchedCity = cities.filter((city) =>
+      city.toLowerCase().city.includes(inputValue.toLowerCase()),
+    );
+    setSuggestion(searchedCity);
+  };
+
+  return (
+    <div className="input">
+      <label htmlFor="input">{hint}</label>
+      <input type="text" id="input" onChange={handleChange} />
+      {suggestion.length > 0 && (
+        <ul>
+          {" "}
+          {suggestion.map((city, index) => (
+            <li key={index}>{city}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Input;
