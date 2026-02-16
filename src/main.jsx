73cad89cb3eb784ba0ebd11a2d3a0395cc2540f3
Hossein Soleimani1 <Hossein.Soleimani@SABALIFT.COM>
2026-02-16 15:31:09 +0330

    useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 
 /* eslint-disable react/prop-types */
 const Input = ({ hint }) => {
@@ -15,31 +15,77 @@ const Input = ({ hint }) => {
     "Tafresh",
     "Sanandaj",
   ];
-  const [suggestion, setSuggestion] = useState([]);
+  const initialState = {
+    value: "",
+    typedLength: 0,
+    shouldSelect: false,
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "SetTyped":
+        return {
+          ...state,
+          value: action.payload.value,
+          typedLength: action.payload.length,
+          shouldSelect: action.payload.shouldSelect,
+        };
+      default:
+        return state;
+    }
+  }
+
+  const inputRef = useRef(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleChange = (event) => {
-    const inputValue = event.target.value;
-    if (inputValue === "") {
-      setSuggestion([]);
-      return;
-    }
-    const searchedCity = cities.filter((city) =>
-      city.toLowerCase().startsWith(inputValue.toLowerCase()),
+    const inputValue = event.target.value.toLowerCase();
+
+    const match = cities.find((city) =>
+      city.toLowerCase().startsWith(inputValue),
     );
-    setSuggestion(searchedCity);
+
+    if (match && inputValue.length < match.length) {
+      dispatch({
+        type: "SetTyped",
+        payload: {
+          value: match,
+          length: inputValue.length,
+          shouldSelect: true,
+        },
+      });
+    } else {
+      dispatch({
+        type: "SetTyped",
+        payload: {
+          value: inputValue,
+          length: inputValue.length,
+          shouldSelect: false,
+        },
+      });
+    }
   };
+
+  useEffect(() => {
+    if (
+      state.shouldSelect &&
+      inputRef.current &&
+      state.typedLength < state.value.length
+    ) {
+      inputRef.current.setSelectionRange(state.typedLength, state.value.length);
+    }
+  }, [state.value, state.typedLength, state.shouldSelect]);
 
   return (
     <div className="input">
       <label htmlFor="input">{hint}</label>
-      <input type="text" id="input" onChange={handleChange} />
-      {suggestion.length > 0 && (
-        <ul>
-          {suggestion.map((city, index) => (
-            <li key={index}>{city}</li>
-          ))}
-        </ul>
-      )}
+      <input
+        ref={inputRef}
+        type="text"
+        value={state.value}
+        id="input"
+        onChange={handleChange}
+      />
     </div>
   );
 };
